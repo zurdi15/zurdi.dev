80fc6d6fc702e2d108840e9dc96c8012985ea222
zurdi <contact@zurdi.dev>
2025-06-23 12:28:25 +0000

    feat: add preview image link for personal project 'romm' in Portfolio section

--- components/Section/Portfolio/Index.vue
+++ components/Section/Portfolio/Index.vue
@@ -7,13 +7,10 @@ const { lgAndUp } = useDisplay();
   <section id="portfolio">
     <div class="section-inner-container ma-auto py-0 px-1">
       <div class="text-white">
-        <SectionChip
-          icon="mdi-image-filter-none"
-          section="portfolio"
-        />
+        <SectionChip icon="mdi-image-filter-none" section="portfolio" />
         <h2
           data-aos="fade-up"
-          class="font-weight-thin"
+          class="font-weight-thin mb-10"
           :class="{ desktop: lgAndUp, mobile: !lgAndUp }"
         >
           <i18n-t keypath="portfolio.personal-projects">
@@ -25,8 +22,41 @@ const { lgAndUp } = useDisplay();
             >
           </i18n-t>
         </h2>
-        <v-row no-gutters class="pl-5"> </v-row>
+        <v-row no-gutters class="pl-1">
+          <v-col cols="12">
+            <a
+              href="https://romm.app"
+              target="_blank"
+              rel="noopener noreferrer"
+              class="preview-img-link"
+            >
+              <div class="preview-img-container mb-5">
+                <v-img
+                  src="/personal_projects/romm-preview.webp"
+                  data-aos="fade-up"
+                  class="preview-img"
+                />
+              </div>
+            </a>
+          </v-col>
+        </v-row>
       </div>
     </div>
   </section>
 </template>
+<style scoped>
+.preview-img-container {
+  border-radius: 30px;
+  overflow: hidden;
+}
+.preview-img {
+  transition: transform 0.3s;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
+.preview-img:hover {
+  transform: scale(1.05);
+  cursor: pointer;
+}
+</style>
